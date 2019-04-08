2cb1c5cb4216030b9f49d15e4003b38495536ae0
Angelo Capossele <33255129+capossele@users.noreply.github.com>
2019-04-08 10:33:20 +0100

    Update dao.go

--- dao/dao.go
+++ dao/dao.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/capossele/zmq-backend/models"
+	"github.com/capossele/zmq-backend/scheme"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
