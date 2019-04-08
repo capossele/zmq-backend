a6efd7895d12d6ae5b2d42855fe38bac717eb8e7
capossele <angelo@Angelo.local>
2019-04-08 12:58:37 +0100

    zmq added

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/capossele/zmq-backend
 require (
 	github.com/gorilla/mux v1.7.1
 	github.com/iotaledger/iota.go v1.0.0-beta.4
+	github.com/zeromq/goczmq v4.1.0+incompatible
 	go.mongodb.org/mongo-driver v1.0.0
 )
--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/capossele/zmq-backend/handlers"
 	"github.com/gorilla/mux"
+	"github.com/iotaledger/iota.go/transaction"
+	"github.com/iotaledger/iota.go/trinary"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	czmq "github.com/zeromq/goczmq"
 )
 
 // DBNAME Database name
@@ -42,7 +48,7 @@ func init() {
 		log.Fatal(err)
 	}
 
-	//fmt.Println("Connected to MongoDB!")
+	fmt.Println("Connected to MongoDB!")
 
 	// Collection types can be used to access the database
 	//db := client.Database(DBNAME)
@@ -75,4 +81,83 @@ func main() {
 	router.HandleFunc("/txs", handlers.DeleteAllTxsEndpoints).Methods("DELETE")
 	fmt.Println("Starting server on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", router))
+
+}
+
+type Tx struct {
+	Hash               trinary.Hash
+	id                 int
+	time               int64
+	cw                 int
+	cw2                int // TODO: to remove, used only to compare different CW update mechanisms
+	ref                []int
+	refHash            []trinary.Hash
+	app                []int
+	appHash            []trinary.Hash
+	bundle             trinary.Hash
+	firstApproval      float64
+	bundleCurrentIndex uint64
+	bundleLastIndex    uint64
+	isTip              bool
+	//trunkHash          trinary.Hash
+	//branchHash         trinary.Hash
+}
+
+func zmqService() {
+	pubEndpoint := "tcp://35.246.106.61:5556"
+
+	topics := "tx"
+
+	subSock, err := czmq.NewSub(pubEndpoint, topics)
+	if err != nil {
+		panic(err)
+	}
+
+	defer subSock.Destroy()
+
+	fmt.Printf("Collecting updates from IRI Node for %s…\n", topics)
+	subSock.Connect(pubEndpoint)
+
+	timestampMap := make(map[trinary.Hash]int64)
+	trytesMap := make(map[trinary.Hash]trinary.Trytes)
+	var timeOffset int64
+	//for i := 0; i < 1000; {
+	for {
+		msg, _, err := subSock.RecvFrame()
+		if err != nil {
+			panic(err)
+		}
+
+		data := strings.Split(string(msg), " ")
+		now := time.Now()
+		nowMillis := now.UnixNano() /// 1000000
+		if timeOffset == 0 {
+			timeOffset = nowMillis
+		}
+
+		if data[0] == "tx" {
+			hash := data[1]
+			timestampMap[hash] = nowMillis - timeOffset
+			fmt.Println(data[0], hash, timestampMap[hash])
+			//i++
+		} else if data[0] == "tx_trytes" {
+			txObject, err := transaction.AsTransactionObject(data[1])
+			if err != nil {
+				//log.Fatal(err)
+				fmt.Println("ERROR on AsTransactionObject", err)
+			}
+			txObject.AttachmentTimestamp = timestampMap[txObject.Hash]
+			trytes, err := transaction.TransactionToTrytes(txObject)
+			if err != nil {
+				//log.Fatal(err)
+				fmt.Println("ERROR on TransactionToTrytes", err)
+			}
+			trytesMap[txObject.Hash] = trytes
+			//i++
+		}
+	}
+	for k, v := range timestampMap {
+		fmt.Println(k, v)
+	}
+	fmt.Println(trytesMap)
 }
